b384fae93e49e200682e8460800b1c734a40966f
arefe sahebgharani <arefesahebgharani@gmail.com>
2024-06-08 11:48:35 +0330

    add tags

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById("menu")
+menu.addEventListener("click" , function(){
+    let menuContainer = document.getElementById("menu-container")
+    menuContainer.classList.toggle("active")
+})
